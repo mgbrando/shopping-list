78ec62be139394b4e3ab0f5fc52d9d4609cee823
mgbrando <mgbrando29@gmail.com>
2016-12-15 10:57:20 -0500

    Added app2.js which replaces app.js for this project

--- app2.js
+++ app2.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
 	//variables
 	var state={
-		items: []
+		items: [{name: 'apples', checked: false}, {name: 'oranges', checked: false}, {name: 'milk', checked: false}, {name: 'bread', checked: false}]
 	}
 
 	//functions
-	var getListItemHtml=function(item, dVal){
+	var getListItemHtml=function(item){
+		var checked = item.checked;
+		var cssAdded;
+		if(checked)
+			cssAdded='shopping-item__checked';
+
 		return '<li>'+
-        			'<span class="shopping-item" data-shopping-item="'+dVal+'">'+item+'</span>'+
+        			'<span class="shopping-item '+cssAdded+'">'+item.name+'</span>'+
         				'<div class="shopping-item-controls">'+
           					'<button class="shopping-item-toggle">'+
             					'<span class="button-label">check</span>'+
@@ -16,8 +21,8 @@ $(document).ready(function(){
             					'<span class="button-label">delete</span>'+
           					'</button>'+
         				'</div>'+
-      				'</li>';
-	}
+      			'</li>';
+	};
 
 	var addItem = function(itemArray, item){
 		/*if(var index=state.items.find(function(element, index){
@@ -27,7 +32,7 @@ $(document).ready(function(){
 			state.items[index].quantity+=1;
 		else*/
 		itemArray.push({name: item, checked: false});
-	}
+	};
 	var checkUncheck=function(itemArray, itemIndex){
 		console.log(itemArray);
 		console.log(itemArray[itemIndex].checked);
@@ -36,72 +41,60 @@ $(document).ready(function(){
 			itemArray[itemIndex].checked=false;
 		else
 			itemArray[itemIndex].checked=true;
-	}
+	};
 	var removeItem = function(itemArray, itemIndex){
-		delete itemArray[itemIndex];
-	}
+		itemArray.splice(itemIndex, 1);
+		console.log(itemArray);
+	};
 
 	var renderAddedListItem=function(item, dVal, listElement){
 		listElement.append(getListItemHtml(item, dVal));
-	}
+	};
 
 	var removeListItem=function(item){
 
-	}
+	};
+
+	var bindListItemHandlers = function(){
+		//check item code
+		$('.shopping-item-toggle').click(function(event){
+			console.log(state);
+			var listItem = $(this).closest('li');
+			console.log(listItem);
+			checkUncheck(state.items, listItem.index());
+			listItem.children('.shopping-item').toggleClass('shopping-item__checked');
+		});
+
+		//remove item code
+		$('.shopping-item-delete').click(function(event){
+			var listItem=$(this).closest('li');
+			removeItem(state.items, listItem.index());
+			console.log(state);
+			listItem.remove();
+		});
+	};
 
 	var renderList = function(listArray, listElement){
-		listElement.empty();
-		var listHtml=listArray.map(function(item, dVal){
-			return getListItemHtml(item.name, dVal);
+		//listElement.empty();
+		var listHtml=listArray.map(function(item){
+			return getListItemHtml(item);
 		});
 		listElement.html(listHtml);
-		reBind();
-	}
-	var reBind = function(){
-			//check item code
-	$('button.shopping-item-toggle').click(function(event){
-		event.stopImmediatePropagation();
-		var item=$(this).closest('div').siblings('span');
-		checkUncheck(state.items, parseInt(item.data('shopping-item')));
-		item.toggleClass('shopping-item__checked');
-	});
-
-	//remove item code
-	$('.shopping-item-delete').click(function(event){
-		removeItem(state.items, parseInt($(this).closest('div').siblings('span').data('shopping-item')));
-		$(this).closest('li').remove();
-	});
+		bindListItemHandlers();
 	};
 
 	//main program
+	
+	renderList(state.items, $('.shopping-list'));
 
-	//add item code
+		//add item code
 	$('#js-shopping-list-form').submit(function(event){
 		event.preventDefault();
 		var item=$('#shopping-list-entry').val().trim();
 		addItem(state.items, item);
-		renderAddedListItem(item, state.items.length-1, $('.shopping-list'));
-		reBind();
 		$('#shopping-list-entry').val('');
+		renderList(state.items, $('.shopping-list'));
 	});
-
-	//check item code
-	/*$('button.shopping-item-toggle').click(function(event){
-		console.log(state);
-		var item=$(this).closest('div').siblings('span');
-		console.log(item);
-		if(checkUncheck(state.items, item.data('shopping-item')));
-		item.toggleClass('shopping-item__checked');
-	});
-
-	//remove item code
-	$('.shopping-item-delete').click(function(event){
-		removeItem(state.items, $(this).closest('div').siblings('span').data('shopping-item'));
-		console.log(state);
-		$(this).closest('li').remove();
-	});*/
-	
-	renderList(state.items, $('.shopping-list'));
 });
 
 /*$(document).ready(function(){
